2194b7f5d77e6943ac5bafdd66416589f26b1b5d
Alex SYK <alexsyk1502@gmail.com>
2022-04-26 02:19:21 -0300

    arquivos

--- CPP/main/string.cpp
+++ CPP/main/string.cpp
@@ -2,7 +2,24 @@
 #include <string>
 
 int main() {
+    //CONCATENATION
     //char nome[50] = {"Alex Se Yoon Koo"};
     //std::cout << nome;
-    std::string greeting = "Hello";
+    std::string firstName = "Alex";
+    std::string lastName = " Se Yoon Koo";
+    //std::string greeting = firstName + " " + lastName;
+    std::string greeting = firstName.append(lastName);
+    std::cout << greeting;
+    //STRING LENGTH
+    std::string text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::cout << "the length is " << text.length(); //or text.size()
+    //ACCESS A STRING
+    std::string myString = "Hello";
+    myString[1] = 'J';
+    std::cout << myString[1];
+    //USER INPUT STRING
+    std::string fullName;
+    std::cout << "Please tell me your full name: ";
+    std::getline (std::cin, fullName);
+    std::cout << "your name is " << fullName;
 }
