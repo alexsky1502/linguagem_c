079f39bdeb61dd87b1670b9eb0d68bfb027bad36
Alex SYK <alexsyk1502@gmail.com>
2022-06-25 01:42:25 -0300

    arquivos

---
+++ CPP/main/struct_pointer2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct Rectangle {
+    int length;
+    int breadth;
+};
+
+struct Rectangle* fun() { //retorna um ponteiro do tipo struct Rectangle
+    struct Rectangle *p; 
+    p = new Rectangle; //recebe o endereço do heap
+
+    (*p).length = 15;
+    p->breadth = 7;
+
+    return p; //retorna o endereço criado no heap
+}
+
+int main() {
+    struct Rectangle *ptr = fun(); //ele recebe o endereço p
+
+    cout << "Length " << ptr->length << endl << "Breadth " << ptr->breadth << endl;
+
+    return 0;
+}
